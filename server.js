e96295a8e3ab4c53b9ba4bcb109f33e8254248f7
Manoj Thapliyal <mnjthapliyal@gmail.com>
2018-04-02 00:14:17 +0530

    Initail commit

---
+++ server.js
@@ -0,0 +1,67 @@
+//configure various routes 
+//thing link route of the website
+//pages
+//start a server binding a port on machine
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+//setup a http request handler
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+
+app.use((req, res, next) => {
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`;
+console.log(log);
+fs.appendFile('server.log', log + '\n', (err) => {
+	if(err){
+		console.log('Unable to append server.log');
+	}
+} );
+next();
+});
+
+// app.use((req,res,next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+return new Date().getFullYear() 
+});
+
+hbs.registerHelper('screamIt', (text) => {
+return text.toUpperCase() 
+});
+
+
+app.get('/', (req, res) => {
+//res.send('Hello Express');
+// res.send({name:'manoj',likes:['city','bikes']});
+
+res.render('home.hbs',{
+	welcomeMessage: 'Welcome',
+	pageTitle: 'home page',
+	// currentYear: new Date().getFullYear()
+});
+
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About page',
+		// currentYear: new Date().getFullYear()
+	});
+
+});
+
+//bind application on port in  machine
+app.listen(3000,()=>{
+	console.log('Server is up on port 3000');
+});
