a9e87a08f5f35426b01ef2a76b53efbe3cc2d168
Manoj Thapliyal <mnjthapliyal@gmail.com>
2018-04-03 17:15:20 +0530

    setup start commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 //setup a http request handler
@@ -62,6 +63,6 @@ app.get('/about', (req, res) => {
 });
 
 //bind application on port in  machine
-app.listen(3000,()=>{
-	console.log('Server is up on port 3000');
+app.listen(port,()=>{
+	console.log(`Server is up on port ${port}`);
 });
